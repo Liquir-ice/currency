5344013a4f8bd834b3defb5e93887691ea671588
liquirice <liquirice@gmail.com>
2015-09-13 21:47:20 +0800

    modify gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,35 @@ require('laravel-elixir-jshint');
 require('laravel-elixir-clean');
 require('laravel-elixir-style-guide');
 require('laravel-elixir-apidoc');
+require('laravel-elixir-postcss');
 
 elixir(function (mix) {
     var port = 8000;
 
-    mix.clean()
-        .sass('*.scss')
+     mix.clean()
         .wiredep()
         .jshint()
-        .sync('resources/assets/js/**/*.js', 'public/js');
+        .sync('resources/assets/js/**/*.*', 'public/js')
+        .sync('resources/assets/img/**/*.*', 'public/img')
+        .postcss('**/*.css', {
+            plugins:[ //postcss's plugins
+              require('postcss-mixins'),
+              require('postcss-nested'),
+              require('postcss-simple-vars'),
+            ]
+        });
 
     if (elixir.config.production) {
         mix.useref({src: false})
             .version(['js/*.js', 'css/*.css'])
-            .styleGuide()
-            .apidoc();
+            .styleGuide();
+
     } else {
         mix.serve({
             port: port
         }).browserSync(null, {
             proxy: 'localhost:' + port,
-            reloadDelay: 2000
+            reloadDelay: 500
         });
     }
 });
